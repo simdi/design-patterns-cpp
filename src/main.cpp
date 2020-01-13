71bbe2c91307f95d6365be64ab58eddffeb30916
Chisimdi Damian Ezeanieto <simdi4iverson@yahoo.co.uk>
2020-01-13 11:02:02 +0100

    Updated.

--- src/main.cpp
+++ src/main.cpp
@@ -10,24 +10,24 @@ int main() {
   /*
   * Memento Pattern
   */
-  // Editor editor;
-  // History history;
-  // editor.Content("John");
-  // history.Push(editor.CreateState());
-  // editor.Content("Doe");
-  // history.Push(editor.CreateState());
-  // editor.Content("Ebere");
-  // editor.Restore(history.Pop());
-  // cout << "Last state: " << editor.Content() << endl;
+  Editor editor;
+  History history;
+  editor.Content("John");
+  history.Push(editor.CreateState());
+  editor.Content("Doe");
+  history.Push(editor.CreateState());
+  editor.Content("Ebere");
+  editor.Restore(history.Pop());
+  cout << "Last state: " << editor.Content() << endl;
 
   /*
   * State Pattern
   */
-  Canvas canvas;
-  SelectionTool *selectionTool;
-  canvas.CurrentTool(selectionTool);
-  canvas.mouseDown();
-  canvas.mouseUp();
+  // Canvas canvas;
+  // SelectionTool *selectionTool;
+  // canvas.CurrentTool(selectionTool);
+  // canvas.mouseDown();
+  // canvas.mouseUp();
 
   return 0;
 }
